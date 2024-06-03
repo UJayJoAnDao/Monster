31ca70ef5570c7a7dc7953d0d533d822a24cc9bf
jexmado <156586116+jexmado@users.noreply.github.com>
2024-06-03 12:06:54 +0800

    前端與後端串接、更新商店、更新home.ejs

--- app.js
+++ app.js
@@ -1,13 +1,55 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes/index');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(cors());
 // Set up middleware
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// 建立資料庫連線
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123',
+    database: 'game_store'
+});
+
+db.connect(err => {
+    if (err) {
+        throw err;
+    }
+    console.log('MySQL Connected...');
+});
+
+// 讀取所有商品
+app.get('/api/products', (req, res) => {
+    let sql = 'SELECT * FROM products';
+    db.query(sql, (err, results) => {
+        if (err) {
+            throw err;
+        }
+        res.json(results);
+    });
+});
+
+// 記錄交易
+app.post('/api/transactions', (req, res) => {
+    const { product_id } = req.body;
+    let sql = 'INSERT INTO transactions (product_id) VALUES (?)';
+    db.query(sql, [product_id], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.json({ message: 'Transaction recorded', transactionId: result.insertId });
+    });
+});
 
 // Set up routes
 app.use('/', routes);
