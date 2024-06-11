9573cbc9a05ff7bdf46e622e5194196f11908652
UJay <plmoknijb1234510@gmail.com>
2024-06-11 17:26:26 +0800

    feat:新增任務頁面、寵物隨機移動功能;修改部分樣式。

--- public/js/home.js
+++ public/js/home.js
@@ -35,7 +35,7 @@ const all_pages = document.querySelectorAll('.page');
 const social_page = document.querySelector('.social-page');
 const sprite_page = document.querySelector('.sprite-page');
 const eggs_page = document.querySelector('.eggs-page');
-const task_page = document.querySelector('.tesk-page');
+const task_page = document.querySelector('.task-page');
 const shop_page = document.querySelector('.shop-page');
 const talent_page = document.querySelector('.talent-page');
 const attack_page = document.querySelector('.attack-page');
@@ -133,3 +133,59 @@ function showCounterPage() {
     counter_page.style.visibility = 'visible';
 }
 // 定義更多功能函數...
+const pet = document.querySelector('.pet-area > .pet'); // 獲取寵物的元素
+
+// 獲取寵物的原始位置
+let x_position = parseInt(window.getComputedStyle(pet).left);
+let y_position = parseInt(window.getComputedStyle(pet).bottom)-100;
+
+const x_step = 2; // 每一步移動的距離
+const y_step = 2;
+const amplitude = 2; // 跳動的幅度
+const L = 10;
+// 創建一個走路的動畫
+const walk = setInterval(() => {
+    let i = 0; 
+    while (i<L) {
+        if(Math.floor(Math.random()*100) < 30) {
+            switch(Math.floor(Math.random()*4)) {
+                case 0:
+                    x_position += x_step;
+                    break;
+                case 1:
+                    x_position -= x_step;
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+            switch(Math.floor(Math.random()*4)) {
+                case 0:
+                    y_position += y_step;
+                    break;
+                case 1:
+                    y_position -= y_step;
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+            // 使用 Math.sin 函數來創建一個正弦波，使寵物在走路時上下跳動
+            let newBottom = y_position + amplitude * Math.sin(x_position / x_step);
+    
+            pet.style.left = x_position + 'px'; // 將新的位置應用到寵物上
+            pet.style.bottom = newBottom + 'px';
+        }
+        // // 確保新的高度不會超過父元素的高度
+        // if (newBottom <= pet.parentElement.offsetHeight - pet.offsetHeight) {
+        // }
+            
+        // 如果寵物已經走到了右邊界，則停止動畫
+        if (x_position >= pet.parentElement.offsetWidth - pet.offsetWidth) {
+            clearInterval(walk);
+        }
+        i++;
+    }
+}, 500); // 每100毫秒移動一次
